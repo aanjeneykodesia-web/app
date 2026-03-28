ae8dfe8fcdd4105d1f00ec4c0a5024df590ac103
aanjeneykodesia-web <aanjeneykodesia@gmail.com>
2026-03-29 03:05:12 +0530

    Create MainActivity.java

---
+++ app/src/main/java/com/example/simplelinkapp/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.simplelinkapp;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.loadUrl("https://aa45e057-9d21-41e5-8fc6-86ca91cc3593-00-2zxatot7tv3cv.riker.replit.dev/");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) webView.goBack();
+        else super.onBackPressed();
+    }
+}
